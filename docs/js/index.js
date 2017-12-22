473a28de0c4555b798f1f102419a80c3a6ead3df
Joaquin Giordano <joaquin_giordano@outlook.com>
2017-12-22 12:51:32 -0300

    Add more info on the BlackJS page

--- docs/js/index.js
+++ docs/js/index.js
@@ -16,3 +16,18 @@ onClick('#download_btn',function(){
 	go('https://joaquingiordano.github.io/BlackJS/js/blackjs.js');
 
 });
+
+
+onClick('#creator_page_btn',function(){
+
+	go('https://joaquingiordano.github.io/');
+
+});
+
+
+onClick('#creator_gh_page_btn',function(){
+
+	go('https://github.com/JoaquinGiordano');
+
+});
+
