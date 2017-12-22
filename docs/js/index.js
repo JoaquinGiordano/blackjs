d75ae7a7a524ade9f5afbfb6ad41432133edce65
Joaquin Giordano <joaquin_giordano@outlook.com>
2017-12-22 11:38:25 -0300

    Add the docs folder to create the page

---
+++ docs/js/index.js
@@ -0,0 +1,18 @@
+
+onClick('#githubpage_btn',function(){
+
+	go('https://github.com/JoaquinGiordano/BlackJS');
+
+});
+
+onClick('#email_btn',function(){
+
+	go('mailto:joaquin_giordano@outlook.com');
+
+});
+
+onClick('#download_btn',function(){
+
+	go('../blackjs.js');
+
+});
