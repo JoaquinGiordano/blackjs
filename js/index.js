45e73ad105ab4f462d367fbff5551b5ccdfd02f3
Joaquin <joaquin_giordano@outlook.com>
2018-06-17 22:44:45 -0300

    bug fixed

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 	getElement('#last-vers').html.add("Last version: " + getData('blackjs-last-vers'));
 
 	getElement('#githubpage_btn').onClick(function(){
-		go('https://github.com/JoaquinGiordano/BlackJS');
+		go('https://github.com/JoaquinGiordano/blackjs');
 	});
 
 	getElement('#email_btn').onClick(function(){
@@ -10,11 +10,11 @@
 	});
 
 	getElement('#download_btn').onClick(function(){
-		go('https://joaquingiordano.github.io/BlackJS/versions/blackjs-'+ getData('blackjs-last-vers') +'.min.js');
+		go('https://joaquingiordano.github.io/blackjs/versions/blackjs-'+ getData('blackjs-last-vers') +'.min.js');
 	});
 
 	getElement('#download_unc_btn').onClick(function(){
-		go('https://joaquingiordano.github.io/BlackJS/versions/blackjs-'+ getData('blackjs-last-vers') +'.js');
+		go('https://joaquingiordano.github.io/blackjs/versions/blackjs-'+ getData('blackjs-last-vers') +'.js');
 	});
 
 	getElement('#creator_page_btn').onClick(function(){
